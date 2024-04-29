101b6ffee36faeb5c21c3bf59eb5f4bc1d6f77cd
CecilRiver <2910347807@qq.com>
2024-04-29 16:25:18 +0800

    类别选择改为从数据库中获取已有类别并进行下拉选择

--- src/views/addCargo.vue
+++ src/views/addCargo.vue
@@ -1,12 +1,14 @@
 <template>
-    <div>
-      <my_header></my_header>
-      <h1>货物信息</h1>
-      <form @submit.prevent="addCargo" style="max-width: 400px; margin: 0 auto;">
+  <div>
+    <my_header></my_header>
+    <h1>货物信息</h1>
+    <form @submit.prevent="addCargo" style="max-width: 400px; margin: 0 auto;">
       <label for="name">货物名称:</label>
       <input type="text" id="name" v-model="cargo.name" style="width: 100%;">
       <label for="category">货物类别:</label>
-      <input type="text" id="category" v-model="cargo.category" style="width: 100%;">
+      <select v-model="selectedCategoryId" style="width: 100%;">
+        <option v-for="category in categories" :value="category.categoryId" :key="category.categoryId">{{ category.categoryName }}</option>
+      </select>
       <label for="num">货物数量:</label>
       <input type="text" id="num" v-model="cargo.num" style="width: 100%;">
       <label for="price">货物单价:</label>
@@ -17,10 +19,83 @@
       <input type="text" id="location" v-model="cargo.location" style="width: 100%;">
       <button type="submit" style="width: 100%;">添加货物</button>
     </form>
-    </div>
-  </template>
-  
-  <style lang="less">
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cargo: {
+        name: "",
+        num: "",
+        price: "",
+        supplier: "",
+        location: "",
+      },
+      categories: [],
+      selectedCategoryId: null,
+    };
+  },
+  mounted() {
+    this.fetchCategories();
+  },
+  methods: {
+    fetchCategories() {
+      axios.get("http://localhost:8082/category/all")
+        .then(response => {
+          this.categories = response.data;
+        })
+        .catch(error => {
+          console.error("Error fetching categories:", error);
+        });
+    },
+    addCargo() {
+      // 验证输入是否为空
+      if (!this.cargo.name || !this.selectedCategoryId || !this.cargo.num || !this.cargo.price || !this.cargo.supplier || !this.cargo.location) {
+        this.$message({
+          message: "请填写完整信息！",
+          type: "warning",
+        });
+        return;
+      }
+
+      // 验证货物数量和单价是否为数字
+      if (isNaN(this.cargo.num) || isNaN(this.cargo.price)) {
+        this.$message({
+          message: "货物数量和单价必须为数字！",
+          type: "warning",
+        });
+        return;
+      }
+
+      // 验证成功，发送请求
+      axios.post("http://localhost:8082/cargo/add", {
+          name: this.cargo.name,
+          categoryId: this.selectedCategoryId,
+          num: this.cargo.num,
+          price: this.cargo.price,
+          supplier: this.cargo.supplier,
+          enterTime: new Date().toISOString(),
+          location: this.cargo.location,
+        })
+        .then(response => {
+          console.log("Add cargo:", response.data.msg);
+          this.$message({
+            message: "添加成功！",
+            type: "success",
+          });
+        })
+        .catch(error => {
+          console.error("Error saving profile:", error);
+        });
+    },
+  },
+};
+</script>
+
+<style lang="less">
 h1 {
   text-align: center;
   margin-bottom: 20px;
@@ -32,7 +107,7 @@ label {
   font-weight: bold;
 }
 
-input[type="text"] {
+input[type="text"], select {
   width: 100%;
   padding: 5px;
   margin-bottom: 10px;
@@ -53,70 +128,3 @@ button:hover {
   background-color: #0056b3;
 }
 </style>
-  
-  <script>
-  import axios from "axios";
-  export default {
-    data() {
-      return {
-        cargo: {
-          name: "",
-          category: "",
-          num: "",
-          price: "",
-          supplier: "",
-          location:"",
-        }
-      };
-    },
-    mounted(){
-    },
-    methods: {
- 
-      addCargo() {
-    // 验证输入是否为空
-    if (!this.cargo.name || !this.cargo.category || !this.cargo.num || !this.cargo.price || !this.cargo.supplier || !this.cargo.location) {
-        this.$message({
-            message: '请填写完整信息！',
-            type: 'warning'
-        });
-        return;
-    }
-
-    // 验证货物数量和单价是否为数字
-    if (isNaN(this.cargo.num) || isNaN(this.cargo.price)) {
-        this.$message({
-            message: '货物数量和单价必须为数字！',
-            type: 'warning'
-        });
-        return;
-    }
-
-    // 验证成功，发送请求
-    axios.post('http://localhost:8082/cargo/add', {
-        name: this.cargo.name,
-        category: this.cargo.category,
-        num: this.cargo.num,
-        price: this.cargo.price,
-        supplier: this.cargo.supplier,
-        enterTime: new Date().toISOString(),
-        location: this.cargo.location
-    })
-    .then(response => {
-        console.log('Add cargo:', response.data.msg);
-        this.$message({
-            message: '添加成功！',
-            type: 'success'
-        });
-    })
-    .catch(error => {
-        console.error('Error saving profile:', error);
-    });
-}
-
-
-
-    }
-  };
-  </script>
-  
